2aa0b2fbd2d6ae20854ad5f55a443d5ac277f4c8
Stefan Keller <stefantkeller@gmail.com>
2017-07-24 23:39:54 +0200

    added numericalunits to benchmark which demonstrates the massive head room still possible in terms of performance (though, nu seems not to perform unit checks (acc. doc, to be investigated!), so that these values cannot be realized realistically)

--- tests/profilers/profile_dimquant_vs_pint_vs_nu.py
+++ tests/profilers/profile_dimquant_vs_pint_vs_nu.py
@@ -18,6 +18,9 @@ import pint
 ureg = pint.UnitRegistry()
 Q_ = ureg.Quantity
 
+import numericalunits as nu
+nu.reset_units()
+
 from dimensionalquantity import Dimensional as D
 from dimensionalquantity import DimQuant as DQ
 
@@ -25,10 +28,12 @@ from dimensionalquantity import DimQuant as DQ
 def multiplying_Q_many_times():
     dq_1m = DQ(1, D({'L':1}))
     pint_1m = 1*ureg.meter
+    nu_1m = 1*nu.m
     normal_int = 1
     for i in range(1000):
         dq_1m *= dq_1m
         pint_1m *= pint_1m
+        nu_1m *= nu_1m
         normal_int *= normal_int
 
 @profile
@@ -36,6 +41,8 @@ def create_many_instances():
     bigN = 1000
     for j in range(bigN): pint_m = Q_('{} m'.format(j))
     for j in range(bigN): pint_kg = Q_('{} kg'.format(j))
+    for j in range(bigN): nu_m = j*nu.m
+    for j in range(bigN): nu_kg = j*nu.kg
     for j in range(bigN): dq_m = DQ('{} m'.format(j))
     for j in range(bigN): dq_kg = DQ('{} kg'.format(j))
     for j in range(bigN): normal_int = int(j)
