1d592b403eac6216346b65b87acd6baf1cd2b807
Stefan Keller <stefantkeller@gmail.com>
2017-09-10 22:00:51 +0200

    write docs for DimQuant methods

--- dimquant.py
+++ dimquant.py
@@ -43,12 +43,45 @@ class DimQuant(BaseDimQuant):
             super(DimQuant, self).__init__(*args, **kwargs)
 
     def __repr__(self):
+        """Example:
+        >>> q = DimQuant('1 m/s')
+        >>> print(q)
+        DimQuant(1, Dimensional({'L':1, 't':-1})"""
         return 'DimQuant({}, {})'.format(self.numeric,self.dimensions)
 
     def __str__(self):
+        """Example:
+        >>> q = DimQuant('1 m/s')
+        >>> str(q)
+        '1 m.s-1'
+        Though, the exact result depends on the registered translator.
+        .. seealso:: 
+            :py:meth: `dimensionalquantity.BasicTranslator`
+            :py:meth: `dimensionalquantity.Translator`
+        """
         unit_string = self._T.reverse_unit_lookup(self.dimensions)
         return ' '.join([str(self.numeric), unit_string])
 
     @classmethod
     def register_translator(cls, translator):
+        """Thanks to the registered translator we can
+        convert a string representation (like '1 m/s') into a `DimQuant`,
+        and also a `DimQuant` into a string;
+        which is more human readable.
+        This is a classmethod,
+        which means all DimQuant instances share the same translator.
+        Typically, this method is called at the beginning of a script;
+        right after registering the corresponding look-up-tables (LUTs)
+        needed for the translator.
+        Without explicitly registering a translator,
+        a default instance of `Translator` is used.
+
+        Args:
+            translator (Translator): an instance capable to
+                interpret the string representations used in a particular script.
+
+        .. seealso:: 
+            :py:meth: `dimensionalquantity.BasicTranslator`
+            :py:meth: `dimensionalquantity.Translator`
+        """
         cls._T = translator
